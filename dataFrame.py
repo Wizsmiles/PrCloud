efb72c135faeb3ea1b5328eb3f150fa879ed451a
Jose Javier Escudero Gómez <doublejay93@gmail.com>
2018-01-11 23:17:25 +0100

    Update
    Fixed dataFrame, now all the day's values are float

--- dataFrame.py
+++ dataFrame.py
@@ -13,13 +13,13 @@ sc = SparkContext(conf = conf)
 sqlContext = SQLContext(sc)
 
 def loadData(myFile):
-    data = myFile.flatMap(lambda l: l.split("\n")).map(lambda l: Row(idEstacion=l[0:8],idParametro=l[8:10], idTecnica=l[10:12], anyo=l[14:16], mes=l[16:18],dia1=l[18:24], dia2=l[24:30], dia3=l[30:36], dia4=l[36:42], dia5=l[42:48],dia6=l[48:54], dia7=l[54:60], dia8=l[60:66], dia9=l[66:72], dia10=l[72:78],dia11=l[78:84], dia12=l[84:90], dia13=l[90:96], dia14=l[96:102], dia15=l[102:108],dia16=l[108:114], dia17=l[114:120], dia18=l[120:126], dia19=l[126:132], dia20=l[132:138],dia21=l[138:144], dia22=l[144:150], dia23=l[150:156], dia24=l[156:162], dia25=l[162:168],dia26=l[168:174], dia27=l[174:180], dia28=l[180:186], dia29=l[186:192], dia30=l[192:198],dia31=l[198:204]))
+    data = myFile.flatMap(lambda l: l.split("\n")).map(lambda l: Row(idEstacion=l[0:8],idParametro=l[8:10], idTecnica=l[10:12], anyo=l[14:16], mes=l[16:18],dia1=float(l[18:23]),dia2=float(l[24:29]),dia3=float(l[30:35]),dia4=float(l[36:41]),dia5=float(l[42:47]),dia6=float(l[48:53]),dia7=float(l[54:59]),dia8=float(l[60:65]),dia9=float(l[66:71]),dia10=float(l[72:77]),dia11=float(l[78:83]),dia12=float(l[84:89]),dia13=float(l[90:95]),dia14=float(l[96:101]),dia15=float(l[102:107]),dia16=float(l[108:113]),dia17=float(l[114:119]),dia18=float(l[120:125]),dia19=float(l[126:131]),dia20=float(l[132:137]),dia21=float(l[138:143]),dia22=float(l[144:149]),dia23=float(l[150:155]),dia24=float(l[156:161]),dia25=float(l[162:167]),dia26=float(l[168:173]),dia27=float(l[174:179]),dia28=float(l[180:185]),dia29=float(l[186:191]),dia30=float(l[192:197]),dia31=float(l[198:203])))
     return data
 
 # 1. Cargo cada dataFrame asociado a un anyo
 
 Schema = []
-
+Schema_Average = []
 for i in range(year):
     aux = ""
     aux2 = i+1
