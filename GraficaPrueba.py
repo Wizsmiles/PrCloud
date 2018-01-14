1ef66d65903a832a7f1abb7802458c6a5d907413
Wizsmiles <smileswiz@gmail.com>
2018-01-14 19:24:44 +0100

    Actualizaciones
    Se ha actualizado la grafica de prueba a una más eficiente, se ha
    actualizado el script que obtiene el dataframe para que devuelva todas
    las medidas en la misma magnitud

--- GraficaPrueba.py
+++ GraficaPrueba.py
@@ -1,32 +1,21 @@
 
 # coding: utf-8
 
-# In[56]:
+# In[20]:
 
 
-import csv, operator
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-with open('D:\Documentos\GitHub\PrCloud\dataFrame_prueba') as csvarchivo:
-    entrada = csv.reader(csvarchivo)
-    ar = []
-    for reg in entrada:
-        ar.append(reg)
-    lista = np.array(ar)
-    columnas = ['idEstacion','idParametro','idTecnica','anyo','mes','dia1','dia2','dia3','dia4','dia5','dia6','dia7','dia8','dia9','dia10','dia11','dia12','dia13','dia14','dia15','dia16','dia17','dia18','dia19','dia20','dia21','dia22','dia23','dia24','dia25','dia26','dia27','dia28','dia29','dia30','dia31']
-    filas = range(len(lista))
-    df = pd.DataFrame(data = lista, columns = columnas, index = filas)
+
+columnas = ['idEstacion','idParametro','idTecnica','anyo','mes','dia1','dia2','dia3','dia4','dia5','dia6','dia7','dia8','dia9','dia10','dia11','dia12','dia13','dia14','dia15','dia16','dia17','dia18','dia19','dia20','dia21','dia22','dia23','dia24','dia25','dia26','dia27','dia28','dia29','dia30','dia31']  
+data = pd.read_csv('https://raw.githubusercontent.com/Wizsmiles/PrCloud/master/tablaFinal', header = None, names = columnas)
+print(data)
     
-    '''
-    alternativa 
-    df = pd.DataFrame.from_csv('D:\Documentos\GitHub\PrCloud\dataFrame_prueba')
-    print(df)
-    '''
-    
-    plt.plot(df.dia29.values, df.dia28.values)
-    plt.show()
+
+plt.plot(data.idParametro.values, data.idTecnica.values)
+plt.show()
 
 
         
